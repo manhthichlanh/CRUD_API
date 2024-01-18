697a3fdacd198687dec5a7516c70d504191603da
Manh Nguyen <=>
2024-01-18 23:40:29 +0700

    first commit

---
+++ src/app/configs/db.config.js
@@ -0,0 +1,15 @@
+import { config } from "dotenv"
+config()
+export default {
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSSWORD,
+    DB: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+}
